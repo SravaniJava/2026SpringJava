b40bb17ed9a3aa611bbff90c491c1832c47a6f86
sravaniPal <sravanigoudjava.com>
2026-01-25 15:32:34 +0530

    8th time method logic is changed from 7 to 8 from jan25sravani branch

--- src/main/java/TestS.java
+++ src/main/java/TestS.java
@@ -12,7 +12,7 @@ public class TestS {
         System.out.println("new method added in 6th time");
     }
     public static  void test7(){
-        System.out.println("new method added in 7th time");
+        System.out.println("new method added in 8th time");
     }
 
     public static void test3(){
