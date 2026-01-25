ffbf7c4d774ea5dda5557524ce8fab7ff5c31a0d
sravaniPal <sravanigoudjava.com>
2026-01-25 13:10:20 +0530

    new method is created from jan25sravani branch

--- src/main/java/TestS.java
+++ src/main/java/TestS.java
@@ -1,4 +1,9 @@
 public class TestS {
+
+    public static void testNew(){
+        System.out.println("hell to every one testNew");
+
+    }
     public static void main(String[] args) {
         System.out.println("this is sravani 2026Test");
         System.out.println("this is sravani java25 addeed from jan25sravani brnach");
