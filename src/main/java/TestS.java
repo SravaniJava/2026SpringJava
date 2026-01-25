cb0d3d22da8722555383e9c64e6934256e3b67d9
sravaniPal <sravanigoudjava.com>
2026-01-25 13:13:08 +0530

    second new  method is created from jan25sravani branch

--- src/main/java/TestS.java
+++ src/main/java/TestS.java
@@ -4,6 +4,9 @@ public class TestS {
         System.out.println("hell to every one testNew");
 
     }
+    public static void test2(){
+        System.out.println("second time added new method");
+    }
     public static void main(String[] args) {
         System.out.println("this is sravani 2026Test");
         System.out.println("this is sravani java25 addeed from jan25sravani brnach");
