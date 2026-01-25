2b86acf7e5f9bf2270dd295825521922c7dac494
sravaniPal <sravanigoudjava.com>
2026-01-25 17:23:40 +0530

    test2 method change

--- src/main/java/TestS.java
+++ src/main/java/TestS.java
@@ -5,7 +5,7 @@ public class TestS {
 
     }
     public static void test2(){
-        System.out.println("third  time add new method");
+        System.out.println("third  time  chnagining in test2 add new method");
         System.out.println("6th time added");
     }
     public static  void test6(){
